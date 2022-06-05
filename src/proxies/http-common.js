665dcb80712b0c8cd1e2b74a3a6919f69b2bf4c5
Osca <osca-ru@hotmail.com>
2022-06-04 22:56:12 -0500

    change url

--- src/proxies/http-common.js
+++ src/proxies/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://montracapi.azurewebsites.net/api',
+    baseURL: 'https://montracapi1.azurewebsites.net/api',
     headers: {
         'Content-type': 'application/json'
     }
